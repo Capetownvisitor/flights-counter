e700ea75447a078edf7fb16a622b42821d7b49f6
Capetownvisitor <Niseb2007@gmail.com>
2026-02-21 00:23:58 +0100

    Add some Touchups to the script and gitignore

--- script.py
+++ script.py
@@ -1,11 +1,10 @@
 from opensky_api import OpenSkyApi, OpenSkyStates, StateVector
 import sys
 
-def get_aircraft_number_by_carrier(carrier_code: str = "DLH") -> int:
+def get_aircraft_number_by_carrier(api: OpenSkyApi,carrier_code: str = "DLH") -> int:
 
     try:
         # Call the API and get all current States / Flights
-        api = OpenSkyApi()
         s: OpenSkyStates = api.get_states()
     except Exception as e:
         print("An error occurred while calling the API")
@@ -23,6 +22,9 @@ def get_aircraft_number_by_carrier(carrier_code: str = "DLH") -> int:
 
     for state in states_vector:
         callsign: str = state.callsign
+        # skip missing callsigns
+        if callsign is None:
+            continue
         onGround: bool = state.on_ground
         if callsign.startswith(carrier_code) and not onGround:
             #found callsign prefix
@@ -31,14 +33,16 @@ def get_aircraft_number_by_carrier(carrier_code: str = "DLH") -> int:
     return num_carrier_planes
 
 if __name__ == "__main__":
+    # create API Object once and pass it over for every function call
+    api = OpenSkyApi()
 
     print("Getting the Data from OpenSky...")
     if len(sys.argv) <= 1:
         # no argmuents supplied:
-        print(f"Currently there are: {get_aircraft_number_by_carrier('DLH')} planes from LuftHansa in the air.")
+        print(f"Currently there are: {get_aircraft_number_by_carrier(api, 'DLH')} planes from LuftHansa in the air.")
     elif len(sys.argv[1]) == 3:
         # try to use comamnd line input as carrier_code:
-        print(f"Currently there are: {get_aircraft_number_by_carrier(sys.argv[1].upper())} planes from your chosen carrier in the air.")
+        print(f"Currently there are: {get_aircraft_number_by_carrier(api, sys.argv[1].upper())} planes from your chosen carrier in the air.")
     else:
         print("Wrong Input Format!")
         print("Use: python script.py 'XXX' <-- 3 letter carrier code")
